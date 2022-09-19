c40bbe95c74649960f0f91d27dcb3af6ebc684e4
Paul <f1rstmarious@yandex.ru>
2022-09-20 00:10:01 +0300

    homework completed

---
+++ zadacha41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int [size];
+Console.WriteLine("Созданный массив: ");
+
+void Input(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int GetCompare(int[] nums)
+{
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+Input(size);
+Console.WriteLine($"Количество положительных элементов: {GetCompare(nums)}");
+
+
